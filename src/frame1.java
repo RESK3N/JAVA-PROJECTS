d6dddc24f77abf2dac5ba2d0622efde59e118fa7
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2025-04-17 01:56:14 +0530

    update

---
+++ src/frame1.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+class abc extends JFrame{
+    abc(){
+        setVisible(true);
+        setSize(400,400);
+        JLabel l1 = new JLabel("Hello World !! ");
+    }
+}
+
+public class frame1 {
+    public static void main(String[] args) {
+        abc a= new abc();
+
+    }
+}
