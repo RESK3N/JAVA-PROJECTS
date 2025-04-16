63b1b2d45176a7b269381c9c74d7ddca9c331355
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2025-04-17 02:04:33 +0530

    update

--- src/frame1.java
+++ src/frame1.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 class abc extends JFrame{
     abc(){
+        setLayout(new FlowLayout());//cardLayout, flowlayout , gridlayout, nulllayout
         setVisible(true);
         setSize(400,400);
         JLabel l1 = new JLabel("Hello World !! ");
