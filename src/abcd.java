be441ffff37d0dbef5578a27082f1d5cc8168367
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2025-11-15 00:54:49 +0530

    update

---
+++ src/abcd.java
@@ -0,0 +1,61 @@
+
+import java.util.*;
+
+class abcd{
+    public static void main(String[] args){
+        int a=10;
+        int b,c,d,e;
+        b=++a;
+        c=a++;
+        d=b+c;
+        e=d++;
+        System.out.println(e);
+
+
+
+
+
+
+
+//        Scanner dad = new Scanner(System.in);
+//        String name = "Aparajeeta";
+//        String title ="Ghosh";
+//        String fname=name+"_"+title;//concatination
+//        System.out.println(fname);
+
+
+
+
+
+
+
+
+
+
+//        System.out.print("enter kms :");
+//        float km = dad.nextFloat();
+//        float mile= 0.6f*km;
+//        System.out.println("Your kms in miles >> "+mile);
+
+
+
+
+
+
+
+
+//        System.out.print("Enter marks of English > ");
+//        float marks1=sc.nextFloat();
+//        System.out.print("Enter marks of Maths > ");
+//        float marks2=sc.nextFloat();
+//        System.out.print("Enter marks of Science > ");
+//        float marks3=sc.nextFloat();
+//        System.out.print("Enter marks of H&G > ");
+//        float marks4=sc.nextFloat();
+//        System.out.print("Enter marks of Computer > ");
+//        float marks5=sc.nextFloat();
+//        float sum=marks1+marks2+marks3+marks4+marks5;
+//        sum=sum/5;
+//        System.out.println("Your Percentage Result is >> "+sum);
+    }
+}
